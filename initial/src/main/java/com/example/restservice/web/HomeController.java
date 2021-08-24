5f3eeda3eb3175510b45f6374c686858307e6bc0
Quang <43954417+quangDecember@users.noreply.github.com>
2021-08-24 09:57:37 -0400

    Authenticating a User with LDAP, show login page but cannot in yet

---
+++ initial/src/main/java/com/example/restservice/web/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.restservice.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+
+}
